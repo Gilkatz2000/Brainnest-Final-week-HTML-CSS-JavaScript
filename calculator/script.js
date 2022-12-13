f0759e9a623ac279d522b57bc864082219002795
Gil Katz <109037085+Gilkatz2000@users.noreply.github.com>
2022-12-13 21:08:16 +0200

    Calculator Buttons Not Working

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+let input = document.getElementById("input");
+
+function display(num) {
+    input.value += num;
+}
+
+function Clear() {
+    input.value = "";
+}
+
+function del() {
+    input.value  = input.value.slice(0, -1);
+
+}
+
+function equal() {
+    try {
+        input.value = eval(input.value);   
+    } catch (error) {
+        alert("Invalid Input!!!");
+    }
+}
